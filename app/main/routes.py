866a66103b57439fd02cef6dbb705e2abaf477d0
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-08-09 17:38:58 +0200

    Add all files

---
+++ app/main/routes.py
@@ -0,0 +1,93 @@
+import os
+import json
+import time
+import pandas as pd
+
+from flask import render_template, request, send_file, after_this_request
+from werkzeug.utils import secure_filename
+from . import main
+
+ALLOWED_EXTENSIONS = {'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@main.route('/')
+def index():
+    """Renders the tpose landing page
+
+    Returns:
+        str: HTML output
+    """
+
+    return render_template('index.html')
+
+@main.route('/do', methods = ["GET", "POST"])
+def do():
+    """Converts the t-scan CSV to Excel (POST only)
+
+    Returns:
+        Response: Flask response which forces the browser to download the Excel file
+    """
+
+    if request.method != "POST":
+        return "This endpoint only accepts POST requests."
+    
+    if "csv" not in request.files:
+        return "Missing file."
+    
+    file = request.files["csv"]
+    if file.filename == '':
+        return "No selected file"
+    
+    cache_dir = os.path.abspath(os.environ["CACHE_DIR"])
+    
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+
+        # Save the file to cache
+        file_path = os.path.join(cache_dir, filename)
+        file.save(file_path)
+
+    # Load into pandas
+    df = pd.read_csv(file_path, sep=",", header=0)
+    
+    # Create Excel filename
+    excel_path = f"{file_path}.xlsx"
+
+    # Build the Excel Writer
+    writer = pd.ExcelWriter(excel_path,
+                        engine='xlsxwriter',
+                        engine_kwargs={'options': {'strings_to_numbers': True}})
+    sheet_name = "t_pose" # funny funny
+
+    df.to_excel(writer, sheet_name=sheet_name)
+
+    workbook  = writer.book
+    worksheet = writer.sheets[sheet_name]   
+
+    # Prepare float and int formats    
+    float_format = workbook.add_format({'num_format': '0.000000'})
+    int_format = workbook.add_format({'num_format': '0'})
+
+    # Set the appropriate formats where needed
+    for index, column in enumerate(df.dtypes):
+        if column == "float64":
+            worksheet.set_column(index, index, None, float_format)
+        elif column == "int64":
+            worksheet.set_column(index, index, None, int_format)
+
+    # Close the Excel file
+    writer.close()
+
+    # After the Excel file has been downloaded, remove the Excel and CSV files
+    @after_this_request
+    def remove_file(response):
+        try:
+            os.remove(excel_path)
+            os.remove(file_path)
+        except Exception as error:
+            return "Oops?"
+        return response
+
+    return send_file(excel_path, as_attachment=True)
---
+++ server.py
@@ -0,0 +1,28 @@
+import os
+import argparse
+from app import create_app
+
+parser = argparse.ArgumentParser(description='Run tpose frontend')
+parser.add_argument('-debug', '--debug', nargs='?', type=bool, default=False, help='output file, in JSON format')
+
+args = parser.parse_args()
+
+debug = args.debug
+
+environment_variables = [ "CACHE_DIR" ]
+
+for environment_variable in environment_variables:
+ 	if not environment_variable in os.environ:
+ 		raise Exception(f"Environment variable '{environment_variable}' missing")
+
+
+app = create_app(debug=debug)
+
+if debug:
+	app.jinja_env.auto_reload = True
+	app.config['TEMPLATES_AUTO_RELOAD'] = True
+	app.run(host='0.0.0.0', debug=debug)
+else:
+	from waitress import serve
+	print("tpose started")
+	serve(app, host='0.0.0.0', port=8080)
